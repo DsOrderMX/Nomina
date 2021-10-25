47d20f482fe554d610ac943bfd6ef38bcbbc9c83
DsOrderMX <cemdlc74@gmail.com>
2021-10-25 14:16:41 -0600

    Creando Migracion de la tabla empleados

---
+++ app/Http/Controllers/EmpleadoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Emplado;
+use Illuminate\Http\Request;
+
+class EmpleadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Emplado  $emplado
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Emplado $emplado)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Emplado  $emplado
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Emplado $emplado)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Emplado  $emplado
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Emplado $emplado)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Emplado  $emplado
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Emplado $emplado)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_10_25_195812_create_empleados_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("codigo");
+            $table->string('nombre');
+            $table->string('apaterno');
+            $table->string('amaterno');
+            $table->string('email');
+            $table->string('tipo_contrato');
+            $table->string('status')->default('activo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+}
